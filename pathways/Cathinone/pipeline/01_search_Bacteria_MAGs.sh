1774ef24086a488d09ccfaef1737d57333e91c27
Jason Stajich <jasonstajich.phd@gmail.com>
2020-07-13 21:56:30 -0700

    search MAGs

--- pathways/Cathinone/pipeline/01_search_Bacteria_MAGs.sh
+++ pathways/Cathinone/pipeline/01_search_Bacteria_MAGs.sh
@@ -4,6 +4,8 @@
 module load hmmer/3
 module unload perl
 module load parallel
+module load muscle
+module load fasttree
 # bacteria MAGs
 
 CPUS=$SLURM_CPUS_ON_NODE
@@ -13,24 +15,29 @@ fi
 CPU=$CPUS
 
 CUTOFF=1e-3
-INPUT=../../processed/EukRep_autometa_Bacteria/MAGs/
-OUT=results
+INPUT=../../processed/EukRep_autometa_Bacteria/MAGs
+parallel -j $CPU esl-sfetch --index {} ::: $(ls $INPUT/*.faa)
+
+OUTTOP=results
 for DIR in Boyce2018 Groves_S2_genes
 do
-  OUT=$OUT/$DIR
-  mkdir -p $OUT
-
-  for HMM in $(ls steps/$DIR/*.hmm)
-  do
-   step=$(basename $HMM .hmm)
-
-   parallel -j $CPU hmmsearch -E $CUTOFF --domtbl $OUT/$step.{/.}.domtbl $HMM {} \> $OUT/$step.{/.}.hmmsearch ::: $(ls $INPUT/*.faa)
-   parallel -j $CPU esl-sfetch --index {} ::: $(ls $INPUT/*.faa)
-   parallel -j $CPU grep -v "^#" $OUT/$step.{/.}.domtbl \| esl-sfetch -f {} - \> $OUT/$step.{/.}.hits.faa ::: $(ls $INPUT/*.faa)
-   for n in $(ls $OUT/*.faa)
-   do
-	     name=$(basename $n .hits.faa)
-	     perl -i -p -e "s/>/>$name|/" $n
-     done
-   done
- done
+    OUT=$OUTTOP/$DIR
+    mkdir -p $OUT
+    
+    for HMM in $(ls steps/$DIR/*.hmm)
+    do
+	step=$(basename $HMM .hmm)	
+	parallel -j $CPU hmmsearch -E $CUTOFF --domtbl $OUT/$step.{/.}.domtbl $HMM {} \> $OUT/$step.{/.}.hmmsearch ::: $(ls $INPUT/*.faa)
+	#parallel -j $CPU grep -v "^#" $OUT/$step.{/.}.domtbl \| awk \'\{print \$1\}\' \| sort \| uniq \>  $step.{/.}.list :::  $(ls $INPUT/*.faa)
+	parallel -j $CPU grep -v "^#" $OUT/$step.{/.}.domtbl \| awk \'\{print \$1\}\' \| sort \| uniq \| esl-sfetch -f {} - \> $OUT/$step.{/.}.hits.faa ::: $(lsx $INPUT/*.faa)
+	find $OUT -size 0 | xargs rm -f
+    done
+    for n in $(ls $OUT/*.faa)
+    do
+	name=$(basename $n .hits.faa)
+	perl -i -p -e "s/>/>$name|/; s/\*//;" $n
+	dir=$(dirname $n)
+	muscle < $n > $dir/$name.hits.fasaln
+	FastTreeMP -wag -gamma < $dir/$name.hits.fasaln > $dir/$name.hits.FT.tre
+    done
+done
